bb08a377d03dd064e5bd377fd7e99a8aed1b1c09
AdrenaRose <Charlenejparker81@gmail.com>
2023-03-19 22:51:02 -0400

    added Q&As 1-5

--- Assets/script.js
+++ Assets/script.js
@@ -8,11 +8,30 @@ var questionOneB = "String";
 var questionOneC = "Key Value";
 var questionOneD = "Boolean";
 
-var questionTwo = "";
-var questionOneA = "Number";
-var questionOneB = "String";
-var questionOneC = "Key Value";
-var questionOneD = "Boolean";
+var questionTwo = "What variable type should be used to store someone's age?";
+var questionTwoA = "Number";
+var questionTwoB = "String";
+var questionTwoC = "Integer";
+var questionTwoD = "Boolean";
+
+var questionThree =
+  "What property would you use to determine if a variable is a string, boolean, or object?";
+var questionThreeA = "kindOf";
+var questionThreeB = "variableType";
+var questionThreeC = "variableName";
+var questionThreeD = "typeOf";
+
+var questionFour =
+  "A sequence of steps that must be taken to perform a task is called a(n):";
+var questionFourA = "Algorithm";
+var questionFourB = "Function";
+var questionFourC = "Process";
+var questionFourD = "Formula";
+
+var questionFive =
+  "A boolean is a variable type that can only hold a value of either true or false.";
+var questionFiveA = "True";
+var questionFiveB = "False";
 
 promptQuestion(
   questionOne,
@@ -33,27 +52,24 @@ promptQuestion(
 );
 
 promptQuestion(
-  question ,
-  question ,
-  question ,
-  question ,
-  question ,
-  question 
-)
+  questionThree,
+  questionThreeA,
+  questionThreeB,
+  questionThreeC,
+  questionThreeD,
+  questionThreeD
+);
 
-promptQuestion(question, question, question, question, question, question);
+promptQuestion(
+  questionFour,
+  questionFourA,
+  questionFourB,
+  questionFourC,
+  questionFourD,
+  questionFourA
+);
 
-promptQuestion(question, question, question, question, question, question);
-
-promptQuestion(question, question, question, question, question, question);
-
-promptQuestion(question, question, question, question, question, question);
-
-promptQuestion(question, question, question, question, question, question);
-
-promptQuestion(question, question, question, question, question, question);
-
-promptQuestion(question, question, question, question, question, question);
+promptTrueFalse(questionFive, questionFiveA, questionFiveB, questionFiveA);
 
 function promptQuestion(question, a, b, c, d, correctAnswer) {
   document.getElementById("Question").append(question);
@@ -61,4 +77,10 @@ function promptQuestion(question, a, b, c, d, correctAnswer) {
   document.getElementById("Choice-B").append(b);
   document.getElementById("Choice-C").append(c);
   document.getElementById("Choice-D").append(d);
+}
+
+function promptTrueFalse(question, a, b, correctAnswer) {
+  document.getElementById("Question").append(question);
+  document.getElementById("Choice-A").append(a);
+  document.getElementById("Choice-B").append(b);
 }
